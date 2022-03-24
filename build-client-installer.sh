530b392c877946b176c145779c81ebf28ae8d506
František Šimorda <frantisek.simorda@gmail.com>
2022-03-24 11:31:29 +0100

    few changes

--- build-client-installer.sh
+++ build-client-installer.sh
@@ -2,11 +2,11 @@
 # need p7zip-full package on Ubuntu
 # after generating client certificate:
 # ./easyrsa gen-req client nopass
-# signing client certificate:
-echo "yes" | ./easyrsa sign-req client $1
+# and after signing client certificate:
+#echo "yes" | ./easyrsa sign-req client $1
 key_dir="/etc/openvpn/server/keys"
 easy_rsa_dir="/etc/openvpn/server/easy-rsa"
-# client_name="company"
+#client_name="company"
 
 mkdir "$key_dir/$1/"                                                               # create user dir
 cp "$easy_rsa_dir/pki/issued/$1.crt" "$key_dir/$1/"                                 # copy certificate
@@ -19,4 +19,4 @@ mv "$key_dir/$1/$1.key" "$key_dir/$1/client.key"
 cd $key_dir
 7z a $1.7z $(find "$key_dir/$1" -type f)
 
-cat 7zS.sfx config.txt $1.7z > $1"_vpn.exe"
+cat $easy_rsa_dir/7zS.sfx $easy_rsa_dir/config.txt $1.7z > $1"_vpn.exe"
